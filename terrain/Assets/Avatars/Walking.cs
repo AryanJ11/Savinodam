9945ee5aa975b392d3c37408c21cca12a1a5c62e
Aryan Jain <aryan110801@gmail.com>
2021-03-30 16:41:48 +0530

    Integrated walking animation into student

---
+++ terrain/Assets/Avatars/Walking.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class Walking : MonoBehaviour {
+
+    void OnAnimatorMove()
+    {
+            Animator animator = GetComponent<Animator>();
+
+            if (animator)
+            {
+     Vector3 newPosition = transform.position;
+               newPosition.z += animator.GetFloat("Runspeed") * Time.deltaTime;
+     transform.position = newPosition;
+            }
+    }
+}
